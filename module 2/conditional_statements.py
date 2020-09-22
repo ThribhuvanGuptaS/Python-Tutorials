fe72badf2c5cccd979c9e21fac264ddb9c02da74
Thribhuvan Gupta S <thribhuvangupta@gmail.com>
2020-09-22 18:05:49 +0530

    Add files via upload

---
+++ module 2/conditional_statements.py
@@ -0,0 +1,38 @@
+# if statements
+a = int(input("A = "))
+b = int(input("B = "))
+
+if a > b and a > 10 :
+    print("a is greater than b")
+    print(f"a value is {a}")
+if b > a:
+    print("b is greater than a")
+    print(f"b value is {b}")
+if b == a:
+    print("b is equal a")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if-else statements
+
+
+# if-elif-else statements
